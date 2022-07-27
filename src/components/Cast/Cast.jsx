bfe24360c2b7f29b2f2d33a442440b58ce384129
Sasha Shkapenko <sasha.shkapenko@gmail.com>
2022-07-27 12:46:08 +0100

    cleans code

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,5 +1,5 @@
 import { getCast } from "services/api";
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import placeholderImg from '../../images/user-icon-placeholder-1.png'
 import s from './Cast.module.css';
